46fa74fa2e077db792d17a2016c1d01cfa5c42eb
Brian Omondi <brianhilsden@gmail.com>
2024-03-14 18:41:08 +0300

    used looping

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const newList=[]
+function writeCards(names,event){
+    for(let i=0;i<names.length;i++){
+       let message=`Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        newList.push(message)
+    }
+    return newList
+}
+function countDown(number){
+    for(let i=number;i>=0;i--){
+        console.log(i)
+    }
+}
